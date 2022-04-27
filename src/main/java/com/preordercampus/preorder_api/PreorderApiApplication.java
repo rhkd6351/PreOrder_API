6045286a1a18eacb44b7a860076b3e25e2205076
yekwang <waspy@naver.com>
2022-04-27 12:41:38 +0900

    project setting

---
+++ src/main/java/com/preordercampus/preorder_api/PreorderApiApplication.java
@@ -0,0 +1,13 @@
+package com.preordercampus.preorder_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreorderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreorderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/preordercampus/preorder_api/PreorderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preordercampus.preorder_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreorderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
